5b4a5aabd24849737226370b29f46c08fd08f81c
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-03-01 20:09:56 +0000

    Use options object

--- buildings.js
+++ buildings.js
@@ -1,19 +1,48 @@
-exports.skyscraper = function(howManyFloors) {
-  var drone = new Drone(self)
-  var floorsWeMade = 0
+var Drone = require('drone')
+var blocks = require('blocks')
 
-  while (floorsWeMade < howManyFloors) {
+var hasOwn = Object.prototype.hasOwnProperty
+
+function assign(dest) {
+  for (var i = 1, l = arguments.length; i < l ; i++) {
+    var src = arguments[i]
+    if (!src) {
+      continue
+    }
+    for (var prop in src) {
+      if (hasOwn.call(src, prop)) {
+        dest[prop] = src[prop]
+      }
+    }
+  }
+  return dest
+}
+
+exports.skyscraper = function(numFloors, options) {
+  numFloors = numFloors || 5
+  options = assign({
+    floor: blocks.stone,
+    glass: blocks.glass_pane,
+    width: 14,
+    length: 14
+  }, options)
+
+  var drone = new Drone(self)
+  var made = 0
+
+  while (made < numFloors) {
     drone
       // Make the floor
-      .up().box(blocks.gold, 14, 1, 14)
+      .box(options.floor, options.width, 1, options.length)
       // Make the windows
-      .up().box0(blocks.glass, 14, 1, 14)
-      .up().box0(blocks.glass, 14, 1, 14)
-      .up().box0(blocks.glass, 14, 1, 14)
+      .up().box0(options.glass, options.width, 1, options.length)
+      .up().box0(options.glass, options.width, 1, options.length)
+      .up().box0(options.glass, options.width, 1, options.length)
+      .up()
 
-    floorsWeMade = floorsWeMade + 1
+    made = made + 1
   }
 
   // Make the roof
-  drone.up().box(blocks.gold, 14, 1, 14)
+  drone.box(options.floor, options.width, 1, options.length)
 }
